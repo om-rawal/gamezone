d2dbeea6965cebd7a9e7dbf45c67d5d89b708324
Om <om.rawal@somaiya.edu>
2022-07-26 21:10:45 +0530

    Added build folder for git deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Homepage from './HomePage';
+// import Homepage from './HomePage';
 
 // function App() {
 //   return (
